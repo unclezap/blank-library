bb098e223d4081476dfa12c9fd473e75c1e6aed4
unclezap <zeb.blackwell@gmail.com>
2020-03-02 17:52:59 -0800

    first models made

---
+++ db/migrate/20200303015209_create_lines.rb
@@ -0,0 +1,10 @@
+class CreateLines < ActiveRecord::Migration[6.0]
+  def change
+    create_table :lines do |t|
+      t.integer :page_id
+      t.string :text
+
+      t.timestamps
+    end
+  end
+end
